8316edc89ccf1fca374908e87c8c26e477deb9b0
v3intruder <148601121+v3intruder@users.noreply.github.com>
2023-10-20 22:39:10 -0400

    Add files via upload
    Tutorial:
    https://www.youtube.com/watch?v=lhGBBwX30uM
    
    
    
    Try:
    x = 3
    a = 2
    b = 3
    wich is 3↑↑3
    or try:
    x=4
    a=1
    b=2
    wich is 4↑2 or 4^2
    
    Help me out, I am not sure if its doing good

---
+++ Knuth's up arrow.py
@@ -0,0 +1,10 @@
+print('3↑↑3=3^3^3=7x10^12')
+print('x ↑*a b')
+e1,e2,e3 = int(input('x= ')),int(input('a= ')),int(input('b= '))
+if e2 > 0:
+    t = e1
+    print()
+    g = (e3 ** (e2 - 1))
+    for i in range(0,g):
+        t **= e3
+print(t)
